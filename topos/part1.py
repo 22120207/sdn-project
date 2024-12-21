a13e195322ded49ba5a4ae7706a0f1b9b55a1a36
Cao Tien Minh <caotienminhktvn3@gmail.com>
2024-12-21 23:10:39 +0700

    fix(topos/part1): fix part1 topo error in not delete controller

--- topos/part1.py
+++ topos/part1.py
@@ -10,18 +10,18 @@ class part1_topo(Topo):
     def build(self):
         s1 = self.addSwitch("s1")
         for i in range(1,5):
-            hostname = f"h{i}"
-            mac_addr = f"00:00:00:00:00:0{i}"
-            ip_addr = "10.0.0.{i}/24"
-            host = self.addHost(host, mac=mac_addr, ip=ip_addr)
-            
+            hostname = "h{index}".format(index=i)
+            mac_addr = "00:00:00:00:00:0{index}".format(index=i)
+            ip_addr = "10.0.0.{index}/24".format(index=i)
+            print(hostname, mac_addr, ip_addr)
+            host = self.addHost(hostname, mac=mac_addr, ip=ip_addr)
             self.addLink(host, s1)
 
 topos = {"part1": part1_topo}
 
 if __name__ == "__main__":
     t = part1_topo()
-    net = Mininet(topo=t, controller=None)
+    net = Mininet(topo=t)
     net.start()
     CLI(net)
     net.stop()
