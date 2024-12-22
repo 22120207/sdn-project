b47d73c26025b3b66ab050f63c55e61acd947de5
Cao Tien Minh <caotienminhktvn3@gmail.com>
2024-12-22 12:06:19 +0700

    mod(part1): modify part1 print and requirements install for pox

--- topos/part1.py
+++ topos/part1.py
@@ -13,9 +13,16 @@ class part1_topo(Topo):
             hostname = "h{index}".format(index=i)
             mac_addr = "00:00:00:00:00:0{index}".format(index=i)
             ip_addr = "10.0.0.{index}/24".format(index=i)
-            print(hostname, mac_addr, ip_addr)
             host = self.addHost(hostname, mac=mac_addr, ip=ip_addr)
             self.addLink(host, s1)
+        
+        topology = """
+        [h1]-----{s1}------[h2]
+        [h3]----/    \-----[h4]
+        """
+
+        print("Creating network topology")
+        print(topology)
 
 topos = {"part1": part1_topo}
 
